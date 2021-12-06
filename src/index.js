3d48f8a75bad4588dba5ebe80a0d2a7866826b31
Tobias Bielohlawek <rngtng@users.noreply.github.com>
2021-12-06 10:03:54 +0100

    Fix and add more tests (#1)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 module.exports = (api) => {
   const YAML = require("yaml");
 
-  // application/x-yaml with an alternative of text/yaml
-  function isYamlContent(string) {
-    return string == 'application/x-yaml'
+  function isYamlContent(headers) {
+    var contentType = headers["Content-Type"] || headers["content-type"];
+    return ((contentType === 'application/x-yaml') || (contentType === 'text/yaml'));
   }
 
   function isYaml(string) {
-    return string.split('\n')[0] == '---'
+    return string.split('\n')[0] === '---';
   }
 
-  api.hooks.onRequest.addHook("signRequest", (request, context) => {
+  api.hooks.onRequest.addHook("parseYamlBody", (request) => {
     var { headers, body } = request;
-    if (!isYamlContent(headers['content-type']) && isYaml(body)) {
-      response.headers["content-type"] = "application/json";
+    if (!isYamlContent(headers) && isYaml(body)) {
+      request.headers["Content-Type"] = "application/json";
       request.body = JSON.stringify(YAML.parse(body));
     }
   });
