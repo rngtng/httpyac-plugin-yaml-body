6235da86135ac33b669197ab7080a175a85506fb
Tobias Bielohlawek <Tobias.Bielohlawek@solarisbank.de>
2022-03-04 22:47:36 +0100

    Bump all dependencies
    + update integration test to not use CLI layer (fails due to ES module style)

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -1,6 +1,30 @@
-const  { cli } = require('httpyac'),
-path = require('path'),
-mockServer = require("mockttp").getLocal();
+const { store, send, io } = require('httpyac'),
+ httpFileStore = new store.HttpFileStore(),
+ fs = require('fs/promises'),
+ { join, isAbsolute, dirname, extname } = require('path'),
+ mockServer = require("mockttp").getLocal();
+
+function initFileProvider() {
+  const fileProvider = io.fileProvider;
+
+  fileProvider.isAbsolute = async (fileName) => isAbsolute(fileProvider.toString(fileName));
+  fileProvider.dirname = (fileName) => dirname(fileProvider.toString(fileName));
+  fileProvider.hasExtension = (fileName, ...extensions) =>
+    extensions.indexOf(extname(fileProvider.toString(fileName))) >= 0;
+    fileProvider.joinPath = (fileName, path) =>
+    join(fileProvider.toString(fileName), path);
+  fileProvider.exists = async (fileName) => {
+    try {
+      return !!(await fs.stat(fileProvider.toString(fileName)));
+    } catch (err) {
+      return false;
+    }
+  };
+  fileProvider.readdir = async (dirname) =>
+    fs.readdir(fileProvider.toString(dirname));
+}
+
+initFileProvider();
 
 describe('httpyac-plugin-yaml-body', () => {
   beforeEach(() => mockServer.start(8080));
@@ -10,14 +34,20 @@ describe('httpyac-plugin-yaml-body', () => {
   jest.spyOn(console, 'info').mockImplementation();
 
   it('parsed body as json', async () => {
-    const endpointMock = await mockServer.forPost("/").thenReply(200, "OK");
+    const endpointMock = await mockServer.forPost("/").thenReply(200, "OK"),
+    fileName = 'test.http',
+    httpFile = await httpFileStore.getOrCreate(
+      fileName,
+      async () => await fs.readFile(join(__dirname, '/test.http'), 'utf8'),
+      0,
+      {
+        workingDir: __dirname,
+      }
+    );
 
-    await cli.execute([
-      '',
-      '',
-      path.join(__dirname, '/test.http'),
-      '-n', 'withYaml'
-    ]);
+    await send({
+      httpFile
+    });
 
     const requests = await endpointMock.getSeenRequests();
     expect(await requests[0].body.getJson()).toEqual({id:1});
